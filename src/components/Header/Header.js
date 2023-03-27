50d8160201163ae1649ad38f97b922922fe27451
Tsvetomir-Nikolov <nikolov8512@gmail.com>
2023-03-27 21:54:09 +0300

    Add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <>
+      <nav class="navbar">
+        <div class="logo">
+          <a href="#">
+            <h1>Dp Shop</h1>
+          </a>
+        </div>
+        <ul class="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Add Product</a>
+          </li>
+          <li>
+            <a href="#">Catalog</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">Register</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
